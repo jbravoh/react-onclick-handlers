087093df69d19ae837f3099180817fa8a5d464c7
Jay-Ann Bravo-Harriott <jbravoharriott@gmail.com>
2021-08-23 14:06:32 +0100

    Solve using different functions

--- src/components/TestButton.tsx
+++ src/components/TestButton.tsx
@@ -1,11 +1,35 @@
 function TestButton(): JSX.Element {
-  const handleClick = () => window.alert("test button");
-
   return (
-    <button onClick={handleClick}>
+    <button onClick={() => console.log("test button")}>
       Make me do something when I'm clicked...
     </button>
   );
 }
 
 export default TestButton;
+
+// function TestButton(): JSX.Element {
+//   const handleClick = () => console.log("test button");
+
+//   return (
+//     <button onClick={handleClick}>
+//       Make me do something when I'm clicked...
+//     </button>
+//   );
+// }
+
+// export default TestButton;
+
+// function TestButton(): JSX.Element {
+//   function handleClick() {
+//     console.log("test button");
+//   }
+
+//   return (
+//     <button onClick={handleClick}>
+//       Make me do something when I'm clicked...
+//     </button>
+//   );
+// }
+
+// export default TestButton;
